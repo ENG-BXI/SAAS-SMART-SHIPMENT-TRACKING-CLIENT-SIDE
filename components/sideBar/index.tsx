12c0f53c23b732a5cb94aabfc9da479b7e36f76f
ENG_BXI <eng.0.bxi@gmail.com>
2026-03-22 02:03:41 +0300

    work in side bar and add all route to it

--- components/sideBar/index.tsx
+++ components/sideBar/index.tsx
@@ -1,24 +1,38 @@
 'use client';
-import {BanknoteIcon, Building2Icon, LogOut, LucideHome, Menu, NotepadText} from 'lucide-react';
+import {BanknoteIcon, Building2Icon, LogOut, LucideHome, Menu, NotepadText, Settings} from 'lucide-react';
 import SideBarLogo from './side-bar-logo';
 import SidebarItem, {ISidebarItem} from './sidebar-item';
 import {useIsMobile} from '@/hooks/use-mobile';
 import {Sheet, SheetContent, SheetTrigger} from '../ui/sheet';
 import {Button} from '../ui/button';
+import {usePathname} from 'next/navigation';
 const listOfSideBarItem: ISidebarItem[] = [
-  {text: 'الرئيسية', icon: <LucideHome />},
-  {text: 'الشركات', icon: <Building2Icon />, isSelected: true},
-  {text: 'الاشتراكات', icon: <BanknoteIcon />},
-  {text: 'الملاحظات', icon: <NotepadText />},
-  {text: 'تسجيل الخروج', icon: <LogOut />}
+  {text: 'الرئيسية', icon: <LucideHome />, link: '/admin'},
+  {text: 'الشركات', icon: <Building2Icon />, link: '/admin/company'},
+  {text: 'الاشتراكات', icon: <BanknoteIcon />, link: '/admin/subscription'},
+  {text: 'الملاحظات', icon: <NotepadText />, link: '/admin/notes'},
+  {text: 'تسجيل الخروج', icon: <LogOut />, link: '/admin/logout'},
+  // Manager Route (Temp)
+  {text: 'الرئيسية', icon: <LucideHome />, link: '/manager'},
+  {text: 'الشحنات', icon: <Building2Icon />, link: '/manager/shipments'},
+  {text: 'العملاء', icon: <BanknoteIcon />, link: '/manager/clients'},
+  {text: 'المسارات', icon: <NotepadText />, link: '/manager/ways'},
+  {text: 'الملاحظات', icon: <LogOut />, link: '/manager/notes'},
+  {text: 'الاعدادات', icon: <Settings />, link: '/manager/settings'}
 ];
 const SideBar = () => {
+  // TODO :
+  // optimize this code
+  // Error active link in nested routes
+  const path = usePathname();
+  const pathName = path.split('/').pop();
+  const isSelected = (link?: string) => link?.split('/').pop() === pathName;
   return (
     <aside className='w-60 bg-[#F9F9F9] py-8 px-4'>
       <SideBarLogo />
       <div className='flex flex-col gap-y-1.5'>
         {listOfSideBarItem.map((item, index) => (
-          <SidebarItem key={index} item={item} />
+          <SidebarItem key={index} item={{...item, isSelected: isSelected(item.link)}} />
         ))}
       </div>
     </aside>
--- components/sideBar/sidebar-item.tsx
+++ components/sideBar/sidebar-item.tsx
@@ -1,19 +1,21 @@
 import {cn} from '@/lib/utils';
+import Link from 'next/link';
 import {ReactNode} from 'react';
 
 export interface ISidebarItem {
   icon: ReactNode;
   text: string;
   isSelected?: boolean;
+  link?: string;
 }
 function SidebarItem({item}: {item: ISidebarItem}) {
-  const {icon, text, isSelected = false} = item;
+  const {icon, text, isSelected = false, link} = item;
   const Icon = icon;
   return (
-    <div className={cn(`flex items-center gap-x-1`, isSelected ? 'bg-custom-primary-color text-white' : 'bg-transparent', 'py-1 px-2 rounded-lg hover:bg-custom-primary-color hover:text-white cursor-pointer')}>
+    <Link href={link || '#'} className={cn(`flex items-center gap-x-1`, isSelected ? 'bg-custom-primary-color text-white' : 'bg-transparent', 'py-1 px-2 rounded-lg hover:bg-custom-primary-color hover:text-white cursor-pointer')}>
       {Icon}
       <h4 className='text-md!'>{text}</h4>
-    </div>
+    </Link>
   );
 }
 
