549711fcae124a2119c5ef764f9087bc1958b513
ENG_BXI <eng.0.bxi@gmail.com>
2026-05-07 17:29:46 +0300

    add manager cant be delete self

--- app/(dashboard)/(manager)/users/_components/delete-user-dialog.tsx
+++ app/(dashboard)/(manager)/users/_components/delete-user-dialog.tsx
@@ -3,9 +3,11 @@ import DeleteDialog from '@/components/dashboard/delete-dialog';
 import {useTransition} from 'react';
 import {DeleteUser} from '../_actions';
 import {toast} from 'sonner';
+import {useMe} from '@/services/me';
 
 const DeleteUserDialog = ({id}: {id: string}) => {
   const [isPending, startTransition] = useTransition();
+  const {data} = useMe();
   function handleOnDelete() {
     startTransition(async () => {
       const {error, message} = await DeleteUser(id);
@@ -13,6 +15,7 @@ const DeleteUserDialog = ({id}: {id: string}) => {
       else toast.success(message);
     });
   }
+  if (id == data?.id) return null;
   return <DeleteDialog title='حذف المستخدم' triggerText='حذف المستخدم' description='هل انت متاكد من حذف المستخدم' isLoading={isPending} onclick={handleOnDelete} />;
 };
 
