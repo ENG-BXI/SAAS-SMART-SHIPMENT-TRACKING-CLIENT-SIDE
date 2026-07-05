f54835185f06b286d821ce7bbbfbd685d877deea
ENG_BXI <eng.0.bxi@gmail.com>
2026-07-05 04:26:55 +0300

    add language in redirect to test hosting

--- lib/parse-req.tsx
+++ lib/parse-req.tsx
@@ -4,6 +4,7 @@ export function parseReq(req: NextRequest) {
   const host = req.headers.get('x-forwarded-host') ?? req.headers.get('host') ?? '';
   // const segments = req.nextUrl.pathname.split('/').filter(Boolean); // يشيل الفراغات
   // const hasLocale = routing.locales.includes(segments[0] as any);
+  const locale = req.nextUrl.pathname.split('/')[1];
   const pathName = req.nextUrl.pathname.split('/')[2];
   const protocol = req.nextUrl.protocol;
   const subdomain = host.split(':')[0].split('.').length > 1 ? host.split(':')[0].split('.')[0] : undefined;
@@ -14,6 +15,7 @@ export function parseReq(req: NextRequest) {
     hasCookie: !!req.cookies.get('token')?.value
   });
   return {
+    locale,
     host,
     pathName,
     protocol,
